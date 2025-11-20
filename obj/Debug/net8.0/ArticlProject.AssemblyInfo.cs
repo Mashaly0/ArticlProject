046e38cf614dc6da8fa9502549ee86dc92821af8
Mashaly0 <abdullahmashaly90@gmail.com>
2025-11-20 10:13:38 +0200

    IdUser

--- obj/Debug/net8.0/ArticlProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/ArticlProject.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArticlProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80fad77052471efcb022f25917c3c900fe3f412f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArticlProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArticlProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
